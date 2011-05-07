42b61f8b438ae749ea5068d10983189e2dc8dd93
Wojciech Topolski <wojciech.topolski@gmail.com>
2011-05-07 18:41:21 +0200

    Add svn repo

--- gen/pl/wtopolski/android/sunsetwidget/R.java
+++ gen/pl/wtopolski/android/sunsetwidget/R.java
@@ -10,10 +10,28 @@ package pl.wtopolski.android.sunsetwidget;
 public final class R {
     public static final class attr {
     }
+    public static final class drawable {
+        public static final int frame_landscape=0x7f020000;
+        public static final int frame_portrait=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int icon_ldpi=0x7f020003;
+        public static final int moon=0x7f020004;
+        public static final int sun=0x7f020005;
+        public static final int sunrise=0x7f020006;
+        public static final int sunset=0x7f020007;
+    }
+    public static final class id {
+        public static final int ImageImageView=0x7f060000;
+        public static final int SunriseTextView=0x7f060001;
+        public static final int SunsetTextView=0x7f060002;
+    }
     public static final class layout {
-        public static final int main=0x7f020000;
+        public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f050000;
+    }
+    public static final class xml {
+        public static final int sunset_widget_provider=0x7f040000;
     }
 }

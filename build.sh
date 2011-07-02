5fd52fd722d8a5e94b67730201ef0eba5afead24
Wojciech Topolski <wojciech.topolski@gmail.com>
2011-07-02 11:00:59 +0200

    Czyszczenie pliku pom.xml

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/sh
+echo "Build..."
+mvn clean install
+
+#echo "Usuwanie..."
+#adb uninstall pl.wtopolski.android.sunsetwidget
+
+echo "Install..."
+adb install -r target/sunset-widget.apk
+
+echo "Start..."
+adb shell am start -a android.intent.action.MAIN -n pl.wtopolski.android.sunsetwidget/.LocationsListActivity
